d39c44f50da11be93ac99ea0bdcd635fef9901f6
Miheev Sergey <miheevserj.77@gmail.com>
2021-05-11 17:26:32 +0300

    end

--- javascript.js
+++ javascript.js
@@ -1,88 +1,31 @@
-function story() {
-
-    let kolobok = {
-        name: 'kolobok',
-        say: function(phrase) {
-            console.log(phrase);
-        }
-    };
-
-    let grandFa = {
-        name: 'Ded',
-        age: 32,
-        say: function(phrase) {
-            console.log(phrase);
-        }
-    };
-
-    let grandMa = {
-        name: 'babyshka',
-        say: function(phrase) {
-            console.log(phrase);
-        }
-    };
-
-    let rabbit = {
-        name: 'rabbit',
-        say: function(phrase) {
-            console.log(phrase);
-        }
-    };
-
-    let wolf = {
-        name: 'volk',
-        say: function(phrase) {
-            console.log(phrase);
-        }
-    };
-
-    let fox = {
-        name: 'lisa',
-        say: function(phrase) {
-            console.log(phrase);
-        }
-    };
-
-
-
-    chapter1(kolobok, grandFa, grandMa);
-    chapter2(kolobok, rabbit);
-    chapter3(kolobok, wolf);
-    chapter4(kolobok, fox);
-
-    console.log('name' in kolobok, 'name' in grandFa, 'name' in grandMa, 'name' in rabbit, 'name' in wolf, 'name' in fox, 'age' in fox);
+window.onload = function() {
+    document.getElementById ("butt_click").onclick = send;
 };
 
+function counter1() {
+    let message_counter = 0;
 
-story();
-
-
-function chapter1(kolobok, grandFa, grandMa) {
-    kolobok.say('Privet');
-    grandFa.say(`Privet ${kolobok.name}`);
-    grandMa.say(`Privet ${kolobok.name}`);
-    grandFa.say(`A ti kyda? ${kolobok.name}`);
-    kolobok.say(`ja V les, ${grandFa.name}`);
-    grandFa.say(`Zachem ? ${kolobok.name}`);
-    kolobok.say(`Na ohoty, ${grandFa.name}`);
+    return message_counter;
 };
 
+function send() {
+    let text_mess = document.getElementById ("inp_mess").value;
 
-function chapter2(kolobok, rabbit) {
-    rabbit.say(`${kolobok.name}, ${kolobok.name} ja tebia siem!`);
-    kolobok.say(`ne nado ${rabbit.name} ja ot babushki yshel ja ot dedushki yshel i ot tebja yjdu`);
+    console.log(text_mess);
+    let div_element = create_buttons();
+ 
+    div_element.text_content = text_mess;
+    console.log(div_element);
+    return text_mess;
+    
+
 };
 
+function create_buttons() {
+    let body = document.body;
+    let div_element = document.createElement('div');
+    div_element.classList.add('back_ground');
+    body.appendChild(div_element);
 
-function chapter3(kolobok, wolf) {
-    wolf.say(`${kolobok.name}, ${kolobok.name} ja tebia siem!`);
-    kolobok.say(`ne nado ${wolf.name} ja ot babushki yshel ja ot dedushki yshel ja ot zaica yshel i ot tebja yjdu`);
-};
-
-
-function chapter4(kolobok, fox) {
-    fox.say(`${kolobok.name}, ${kolobok.name} ja tebia siem!`);
-    kolobok.say(`ne nado ${fox.name} ja ot babushki yshel ja ot dedushki yshel ja ot zaica yshel ja ot volka yshel i ot tebja yjdu`);
-    fox.say(`da ? a nu posmotri kto tam za spinoj y tebia ${kolobok.name}`);
-    kolobok.say('aaa noga(((');
+    return div_element;
 };
