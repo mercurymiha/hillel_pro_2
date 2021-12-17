4d1699daf387880d1782b992692e5b18a41d8235
mercurymiha <miheevserj.77@gmail.com>
2021-12-17 17:40:30 +0200

    chatBot

--- javascript.js
+++ javascript.js
@@ -1,40 +1,166 @@
-window.onload = function() {
-    document.getElementById ("butt_click").onclick = send;
-};
-function send() {
-    let text_mess = document.getElementById ("inp_mess").value;
-    let message_delete = document.getElementById ("check_delete");
 
-    if (message_delete.checked) {
-        console.log("checked");
-
-        setTimeout(deleting_message, 5000);
-    } else {
-        console.log("unchecked");
+class Chat {
+    constructor() {
+        let sendButton = document.getElementById('sendButton');
+        sendButton.addEventListener('click', () => {
+            this.sandMessage();
+        });
     }
 
-    console.log(text_mess);
-    let div_element = create_buttons();
- 
-    div_element.text_content = text_mess;
-    console.log(div_element);
-    return text_mess;
+
     
+    sandMessage() {
+        let text = this.getInputMessage();
 
-};
-function deleting_message () {
-    alert('Удаление тест');
-    let test = document.getElementsByClassName("back_ground");
-    while ((elem = test[0])) { 
-        elem.parentNode.removeChild(elem);
-      }
-};
+        if(text){
+            let params = {
+                text: text,
+                backgroundColor: 'red',
+            };
 
-function create_buttons() {
-    let body = document.body;
-    let div_element = document.createElement('div');
-    div_element.classList.add('back_ground');
-    body.appendChild(div_element);
+            let messageBlock = this.createMessageBlock(params);
+            this.addMessageToDialog(messageBlock);  
+        }
+    }
 
-    return div_element;
-};
+    addMessageToDialog(messageBlock) {
+        let messageHistoryBlock = document.getElementById('messageHistory');
+        messageHistoryBlock.appendChild(messageBlock);
+    }
+
+    getInputMessage() {
+        let inputElement = document.getElementById('msg');
+        let inputeValue = inputElement.value;
+        inputElement.value = '';
+
+        return inputeValue;
+    }
+
+
+    createMessageBlock(params) {
+        let messageBlock = document.createElement('div');
+        messageBlock.classList.add('message');
+
+        let messageDateBlock = this.createMessageDateBlock();
+        messageBlock.appendChild(messageDateBlock);
+
+        let messageAvatarBlock = this.createMessageAvatarBlock();
+        messageBlock.appendChild(messageAvatarBlock);
+
+        let messageTextBlock = this.createMessageTextBlock(params.text);
+        messageBlock.appendChild(messageTextBlock);
+
+
+        return messageBlock;
+    }
+
+
+    createMessageDateBlock() {
+        let messageDateBlock = document.createElement('span');
+        messageDateBlock.classList.add('date');
+        messageDateBlock.textContent = '13.04.89';
+
+
+        return messageDateBlock;
+    }
+
+
+    createMessageAvatarBlock() {
+        let messageAvatarBlock = document.createElement('div');
+        messageAvatarBlock.classList.add('avatar');
+
+
+        return messageAvatarBlock;
+
+    }
+
+
+    createMessageTextBlock(text) {
+        let messageTextBlock = document.createElement('p');
+        messageTextBlock.textContent = text;
+
+
+        return messageTextBlock;
+
+    }
+}
+
+class bot {
+    constructor() {
+        this.backgroundColor = 'red';
+    }
+
+    say (phrase) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(`${this.name}: ${phrase}`);
+            }, 1000);
+        });
+    }
+}
+
+
+class KolobokBot extends bot {
+    constructor() {
+        super();
+        this.name = 'kolobok';
+    }
+}
+
+
+class GrandFaBot extends bot {
+    constructor() {
+        super();
+        this.name = 'GrandFa';
+        this.backgroundColor = 'green';
+    }
+}
+
+
+class Story {
+    constructor() {
+
+    }
+
+    startDialog(chat, bot1, bot2) {
+        this.addBotDialogMessage(chat, bot1, 'privet').then(() => {
+            return this.addBotDialogMessage(chat, bot2, 'hello');
+        })
+        .then(() => {
+            return this.addBotDialogMessage(chat, bot1, 'Ny privet');
+        });
+    }
+
+
+    addBotDialogMessage(chat, bot, messageText) {
+        return bot.say(messageText).then((messageText) => {
+            let messageBlock1 = chat.createMessageBlock({
+                text: messageText,
+                backgroundColor: bot.backgroundColor,
+            });
+    
+            chat.addMessageToDialog(messageBlock1);
+        });
+    }
+}
+
+
+let story = new Story();
+
+let chat = new Chat();
+
+
+let kolobok = new KolobokBot();
+let grandFa = new GrandFaBot();
+
+story.startDialog(chat, kolobok, grandFa);
+
+
+function dialog() {
+    chat.createMessageBlock({
+        text: kolobok.say(),
+    });
+
+    grandFa.say();
+}
+
